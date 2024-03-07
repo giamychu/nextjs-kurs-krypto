062d8171f0aca7cdb259da82b6b4e8aa8361198c
giamychu <gich002@student.kristiania.no>
2024-03-07 18:44:14 +0100

    Hei

--- src/crypto/currencies.tsx
+++ src/crypto/currencies.tsx
@@ -7,4 +7,21 @@ export const currencies: CryptoCurrency[] = [
     name: "Dogecoin",
     icon: <Icon component="img" src="/icons/doge.svg" alt="Dogecoin" />,
   },
+  {
+    symbol: "DOGE",
+    name: "Bitcoin",
+    icon: <Icon component="img" src="/icons/uniswap-uni-logo.svg" alt="Dogecoin" />,
+  },
+  {
+    symbol: "DOGE",
+    name: "Solana",
+    icon: <Icon component="img" src="/icons/solana-sol-logo.svg" alt="Dogecoin" />,
+  },
+  {
+    symbol: "DOGE",
+    name: "Aptos",
+    icon: <Icon component="img" src="/icons/aptos-apt-logo.svg" alt="Dogecoin" />,
+  },
+  
+
 ];
